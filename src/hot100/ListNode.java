4a076961a7add7a1d8a091d4ee01316718679b6a
anyangdp@aliyun <anyangdp@aliyun>
2019-08-29 16:27:57 +0800

    hot100-两数相加

---
+++ src/hot100/ListNode.java
@@ -0,0 +1,15 @@
+package hot100;
+
+/**
+ * @Author anyang
+ * @CreateTime 2019/8/29
+ * @Des
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/hot100/两数相加.java
@@ -0,0 +1,96 @@
+package hot100;
+
+/**
+ * @Author anyang
+ * @CreateTime 2019/8/29
+ * @Des
+ */
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+//
+//        如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+//
+//        您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/add-two-numbers
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+//    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//            输出：7 -> 0 -> 8
+//            原因：342 + 465 = 807
+
+import org.junit.Test;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class 两数相加 {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+    @Test
+    public void test1() {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(5);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        ListNode node = addTwoNumbers(l1, l2);
+        while (null != node) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    @Test
+    public void test2() {
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = l1;
+        System.out.println(l2.val);
+        l1.next = new ListNode(1);
+        l1 = l1.next;
+        System.out.println(l2.next.val);
+    }
+}
